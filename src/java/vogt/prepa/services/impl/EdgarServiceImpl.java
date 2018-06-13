ea377cb924c00782d39fad2af5969721621c1a5a
12Lions <12Lions@DESKTOP-HI1UKMT>
2018-06-13 13:04:27 +0100

    PointagesDUnJour OK

--- src/java/vogt/prepa/services/impl/EdgarServiceImpl.java
+++ src/java/vogt/prepa/services/impl/EdgarServiceImpl.java
@@ -11,16 +11,22 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 import vogt.prepa.entities.Individu;
 import vogt.prepa.entities.Pointage;
 import vogt.prepa.services.EdgarService;
+import vogt.prepa.utils.HibernateUtil;
 
 /**
  *
  * @author 12Lions
  */
 public class EdgarServiceImpl implements EdgarService {
-
+    
+    /*
+    *   Retoure uniquement la partie heure (En String)
+    */
     @Override
     public String ExtraireHeure(Date date) {
         DateFormat dateFormat = new SimpleDateFormat("hh:mm:ss");
@@ -28,6 +34,19 @@ public class EdgarServiceImpl implements EdgarService {
         return heure;
     }
 
+    /*
+    *   Retoure uniquement la partie date (En String)
+    */
+    @Override
+    public String ExtraireDate(Date date) {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String dat = dateFormat.format(date);
+        return dat;
+    }
+
+    /*
+    *   Reset la partie heure à 00h:00m:00s
+    */
     @Override
     public Date getDebutdeJournee(Date journee) {
         Calendar calendar = Calendar.getInstance();
@@ -39,6 +58,9 @@ public class EdgarServiceImpl implements EdgarService {
         return calendar.getTime();
     }
 
+    /*
+    *   Modifie la partie heure à 23h:59m:00s
+    */
     @Override
     public Date getFindeJournee(Date journee) {
         Calendar calendar = Calendar.getInstance();
@@ -47,32 +69,54 @@ public class EdgarServiceImpl implements EdgarService {
         calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MINUTE, 59);
         calendar.set(Calendar.HOUR, 11);
-
         return calendar.getTime();
     }
 
+    /*
+    *  h1 >= h2 ? renvoie true si oui false sinon
+    */
     @Override
     public boolean ComparerHeure(String heure1, String heure2) {
-        boolean superieur = false;
-        if ((heure1.compareTo(heure2) > 0) || (heure1.compareTo(heure2) == 0)) {
-            superieur = true;
+        boolean plusGrandouEgal = false;
+        if ((heure1.compareTo(heure2) >= 0)) {
+            plusGrandouEgal = true;
         }
-        return superieur;
+        return plusGrandouEgal;
     }
 
+    /*
+    *  renvoie false si on est dimanche
+    */
     @Override
     public boolean JourDeClasse(Date dateDuJour) {
         boolean dim = true;
         String nomJour = new SimpleDateFormat("EEEE").format(dateDuJour);
-        if (nomJour.equals("dimanche")){
+        if (nomJour.equals("dimanche")) {
             dim = false;
-        }        
+        }
         return dim;
     }
-
+    
+    /*
+    *  renvoie la liste des pointage d'un jour precis
+    */
     @Override
     public List<Pointage> PointagesDUnJour(Date dateDuJour) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        Date debutJournee = getDebutdeJournee(dateDuJour);
+        Date finJournee = getFindeJournee(dateDuJour); 
+        
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.getTransaction().begin();
+        
+        List<Pointage> pointagesJour = session.createCriteria(Pointage.class)
+                .add(Restrictions.between("heure", debutJournee, finJournee))
+                    .list();
+        
+        session.getTransaction().commit();
+        session.close();
+        
+        return pointagesJour;
     }
 
     @Override
