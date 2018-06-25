f11477e4d605dd2c9fe971d9538661658d05a109
Muanza <muanza.kangudie@gmail.com>
2018-06-25 09:04:38 +0100

    Petit service pour David

--- src/java/vogt/prepa/services/DavidService.java
+++ src/java/vogt/prepa/services/DavidService.java
@@ -32,6 +32,7 @@ public interface DavidService {
     public static final String CHEMIN_DU_DOSSIER = "C:\\Users\\muanz\\OneDrive\\Documents\\SIA";
     
     //La fonction doit récupérer un texte du format si dessus et le transformer en liste de pointages
+    //La première colonne représente le matricule
     public List<Pointage> textToPointages(String texte);
     
     
