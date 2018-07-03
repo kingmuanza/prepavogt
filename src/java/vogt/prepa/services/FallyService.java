bf6a14a36f5a73ad965344a19f00eaf1cc71868d
N9-T <tonysimonadji@gmail.com>
2018-07-03 15:04:09 +0200

    Défition et implémentation des méthodes de FallyService

--- src/java/vogt/prepa/services/FallyService.java
+++ src/java/vogt/prepa/services/FallyService.java
@@ -5,10 +5,31 @@
  */
 package vogt.prepa.services;
 
+import java.util.Date;
+import java.util.List;
+import vogt.prepa.entities.Classe;
+import vogt.prepa.entities.Etudiant;
+import vogt.prepa.entities.Individu;
+import vogt.prepa.entities.Pointage;
+
 /**
  *
  * @author muanz
  */
 public interface FallyService {
     
+    //renvoi la liste des pointages d'une classe entre deux dates précises
+    public List<Pointage> getByClasse(Classe classe, Date date1, Date date2);
+    
+    //retourne sous forme de String la moyenne des pointages d'une classe entre deux dates
+    public String moyennePointageParClasse(Classe classe, Date date1, Date date2);
+    
+    //retourne sous forme de String les moyenne des pointages d'un individu entre deux dates
+    public String monyennePointageParIndividu(Individu individu, Date date1, Date date2);
+    
+    //retourne sous forme de String les moyenne des pointages d'un individu entre deux dates
+    public int totalRetardParIndividu(Individu individu, Date date1, Date date2);
+    
+    //renvoie les etudiants qui sont toujours à l'heure entre deux dates
+    public List<Etudiant> toujoursPonctuel(Date date1, Date date2);
 }
