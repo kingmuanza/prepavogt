765f17421c7f549fa34294852f85ee5ca7c7be95
12Lions <12Lions@DESKTOP-HI1UKMT>
2018-07-08 11:50:38 +0100

    estAbsent

--- src/java/vogt/prepa/services/EdgarService.java
+++ src/java/vogt/prepa/services/EdgarService.java
@@ -3,7 +3,6 @@ package vogt.prepa.services;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 import vogt.prepa.entities.Individu;
 import vogt.prepa.entities.Pointage;
 
--- src/java/vogt/prepa/services/impl/EdgarServiceImpl.java
+++ src/java/vogt/prepa/services/impl/EdgarServiceImpl.java
@@ -460,12 +460,10 @@ public class EdgarServiceImpl implements EdgarService {
         return retardsPointageByMatriculeIndiv;
     }
 
-    //***********************************************ODAY
-    
     @Override
     public List<Integer> getDiffHeureMinutes(String heure1, String heure2) {
         List<Integer> diffheureEtMinutes = new ArrayList<>();
-        
+
         String[] fractions1 = heure1.split(":");
         String[] fractions2 = heure2.split(":");
         Integer heureDebut = Integer.parseInt(fractions1[0]);
@@ -474,7 +472,7 @@ public class EdgarServiceImpl implements EdgarService {
         Integer minutesFin = Integer.parseInt(fractions2[1]);
         int heureDiff = heureFin - heureDebut;
         int minutesDiff = minutesFin - minutesDebut;
-        
+
         if (minutesDiff < 0) {
             minutesDiff = 60 + minutesDiff;
             heureDiff--;
@@ -482,75 +480,84 @@ public class EdgarServiceImpl implements EdgarService {
         if (heureDiff < 0) {
             heureDiff = 24 + heureDiff;
         }
-        
+
         diffheureEtMinutes.add(heureDiff);
         diffheureEtMinutes.add(minutesDiff);
-        
+
         return diffheureEtMinutes;
     }
 
     @Override
     public List<Long> cummulDesRetards(String matricule, Date dateDebut, Date dateFin) {
-        
+
         List<Long> differenceTemps = new ArrayList<>();
-        
+
         Long heures = 0L;
         Long minutes = 0L;
-        
-        List<Pointage> tousPointageEnRetardsMatriculeDeuxDates = retardsPointagesEntreDeuxDates(matricule, dateDebut, dateFin);        
-        
-        if(tousPointageEnRetardsMatriculeDeuxDates != null){
+
+        List<Pointage> tousPointageEnRetardsMatriculeDeuxDates = retardsPointagesEntreDeuxDates(matricule, dateDebut, dateFin);
+
+        if (tousPointageEnRetardsMatriculeDeuxDates != null) {
             ListIterator<Pointage> it = tousPointageEnRetardsMatriculeDeuxDates.listIterator();
-            
+
             while (it.hasNext()) {
-                Pointage p = it.next();                
+                Pointage p = it.next();
                 heures += getDiffHeureMinutes(HEURE_ARRIVEE, ExtraireHeure(p.getHeure())).get(0);
                 minutes += getDiffHeureMinutes(HEURE_ARRIVEE, ExtraireHeure(p.getHeure())).get(1);
-            }            
+            }
         }
         differenceTemps.add(heures);
         differenceTemps.add(minutes);
-        
+
         return differenceTemps;
     }
 
     @Override
     public List<Long> cummulDesRetards(Individu individu, Date dateDebut, Date dateFin) {
         List<Long> differenceTempsIndiv = new ArrayList<>();
-        
+
         Long heures = 0L;
         Long minutes = 0L;
-        
-        List<Pointage> tousPointageEnRetardsIndivDeuxDates = retardsPointagesEntreDeuxDates(individu, dateDebut, dateFin);        
-        
-        if(tousPointageEnRetardsIndivDeuxDates != null){
+
+        List<Pointage> tousPointageEnRetardsIndivDeuxDates = retardsPointagesEntreDeuxDates(individu, dateDebut, dateFin);
+
+        if (tousPointageEnRetardsIndivDeuxDates != null) {
             ListIterator<Pointage> it = tousPointageEnRetardsIndivDeuxDates.listIterator();
-            
+
             while (it.hasNext()) {
-                Pointage p = it.next();                
+                Pointage p = it.next();
                 heures += getDiffHeureMinutes(HEURE_ARRIVEE, ExtraireHeure(p.getHeure())).get(0);
                 minutes += getDiffHeureMinutes(HEURE_ARRIVEE, ExtraireHeure(p.getHeure())).get(1);
-            }            
+            }
         }
         differenceTempsIndiv.add(heures);
         differenceTempsIndiv.add(minutes);
-        
+
         return differenceTempsIndiv;
     }
 
+    //***********************************************ODAY
     @Override
     public boolean estAbsent(Individu individu, Date jourLa) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Boolean abs = false;
+        List<Pointage> pointageJournée = PointagesDUnJourPourUnIndividu(individu, jourLa);
+
+        if (pointageJournée != null && pointageJournée.isEmpty()) {
+            abs = true;
+        }
+        return abs;
     }
 
     @Override
     public List<String> matriculesAbsentsUnJourLa(Date jourLa) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
     }
 
     @Override
     public List<Individu> individusAbsentsUnJourLa(Date jourLa) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
     }
 
     @Override
@@ -592,7 +599,5 @@ public class EdgarServiceImpl implements EdgarService {
     public Map<Individu, Integer> nombreAbsencesIndividus(Date dateDebut, Date dateFin) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
-    
 
 }
